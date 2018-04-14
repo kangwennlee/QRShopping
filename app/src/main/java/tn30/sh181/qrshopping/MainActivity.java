f4fe4106af1a8a407a78754a2bc39e0169dbe94b
Lee Kang Wenn <kangwenn@gmail.com>
2018-04-14 14:41:17 +0800

    Initial commit

---
+++ app/src/main/java/tn30/sh181/qrshopping/MainActivity.java
@@ -0,0 +1,13 @@
+package tn30.sh181.qrshopping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
